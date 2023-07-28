0693871ebc8e54f73d59a53db486386e7b6e1964
Gerson Motta <gerson.motta@bairesdev.com>
2023-07-27 21:39:20 -0300

    1) Smallest Missing Positive Integer -- algorithm Write a function:
    class Solution { public int solution(int[] A); }
    
    that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
    
    For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
    
    Given A = [1, 2, 3], the function should return 4.
    
    Given A = [−1, −3], the function should return 1.
    
    Write an efficient algorithm for the following assumptions:
    
    N is an integer within the range [1..100,000];
    each element of array A is an integer within the range [−1,000,000..1,000,000].
    
    2) PermMissingElem - algorithm
    
    You are given an array A of N integers, where N is a positive integer. The array contains integers in the range from 1 to N + 1. However, one element is missing from the array, and your task is to find that missing element.
    
    To clarify, the array A is guaranteed to contain N distinct integers. The missing element is an integer that should be in the range from 1 to N + 1, but it is not present in the array.
    
    Your goal is to implement a function solution(int[] A) that takes the array A as input and returns the value of the missing element.
    
    For example, given an array A with N = 4 such that:
    A[0] = 2
    A[1] = 3
    A[2] = 1
    A[3] = 5
    
    The function should return 4, as it is the missing element in the range from 1 to N + 1.
    
    The function should handle efficient algorithms for cases where N can vary and can be as large as 100,000. Additionally, the elements of the array A are all distinct and within the range from 1 to N + 1.

---
+++ CodeChallenge/CodeChallenge/Classes/TimeComplexity.cs
@@ -0,0 +1,94 @@
+﻿using System.ComponentModel;
+using System;
+
+namespace CodeChallenge.Classes
+{
+    internal static class TimeComplexity
+    {
+        /// <summary>
+        /// Write a function:
+        /// class Solution { public int solution(int[] A); }
+        /// that, given an array A of N integers, returns the smallest positive integer(greater than 0) that does not occur in A.
+        /// For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+        /// 
+        /// Given A = [1, 2, 3], the function should return 4.
+        /// 
+        /// Given A = [−1, −3], the function should return 1.
+        /// 
+        /// Write an efficient algorithm for the following assumptions:
+        /// 
+        /// N is an integer within the range[1..100, 000];
+        /// each element of array A is an integer within the range[−1, 000, 000..1, 000, 000].
+        /// </summary>
+        /// <param name="aArray">Given int array</param>
+        /// <returns></returns>
+        public static int SmallestMissingPositiveInteger(int[] aArray)
+        { 
+            HashSet<int> positiveIntegerLst = new HashSet<int>();
+
+            foreach (var item in aArray)
+            {
+                if(item > 0)
+                    positiveIntegerLst.Add(item);
+            }
+
+            var smallestInteger = 1;
+            while (positiveIntegerLst.Contains(smallestInteger))
+            {
+                smallestInteger++;
+            }
+
+            return smallestInteger;
+        }
+
+        /// <summary>
+        /// Write a function:
+        /// class Solution { public int solution(int[] A); }
+        /// that, given an array A of N integers, returns the smallest positive integer(greater than 0) that does not occur in A.
+        /// For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+        /// 
+        /// Given A = [1, 2, 3], the function should return 4.
+        /// 
+        /// Given A = [−1, −3], the function should return 1.
+        /// 
+        /// Write an efficient algorithm for the following assumptions:
+        /// 
+        /// N is an integer within the range[1..100, 000];
+        /// each element of array A is an integer within the range[−1, 000, 000..1, 000, 000].
+        /// </summary>
+        /// <param name="aArray">Given int array</param>
+        /// <returns></returns>
+        public static int SmallestMissingPositiveIntegerNoHash(int[] aArray)
+        {
+            aArray.ToList().RemoveAll(num => num < 0);
+            
+            var smallestInteger = 1;
+            while (aArray.ToList().Contains(smallestInteger))
+            {
+                smallestInteger++;
+            }
+
+            return smallestInteger;
+        }
+
+        /// <summary>
+        /// Finds the missing element in an array of distinct integers in the range [1..(N + 1)].
+        /// The array contains integers from 1 to N + 1, but exactly one element is missing.
+        /// </summary>
+        /// <param name="A">The input array of distinct integers.</param>
+        /// <returns>The missing element in the range from 1 to (N + 1).</returns>
+        public static int PermMissingElem(int[] aArray) 
+        {
+            int N = aArray.Length;
+            long expectedSum = (long)(N + 1) * (N + 2) / 2;
+            long actualSum = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                actualSum += aArray[i];
+            }
+
+            return (int)(expectedSum - actualSum);
+        }
+    }
+}
